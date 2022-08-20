2cb014ae96c08529fdbb79d72a1a10390a02f0c5
Santiago Viana Vera <salviega6@gmail.com>
2022-08-20 17:27:49 -0500

    [FEAT] In the PensionWallet component was added a 'loading'

--- src/components/PensionWallet/index.js
+++ src/components/PensionWallet/index.js
@@ -1,34 +1,35 @@
 import './PensionWallet.scss'
 import React from 'react';
 import { ethers } from 'ethers'
-import { PensionLoading } from '../PensionLoading';
 import abiProofOfHumanity from '../../blockchain/environment/ProofOfHumanity/abi-proof-of-humanity.json'
 import addressProofOfHumanity from '../../blockchain/environment/ProofOfHumanity/address-proof-of-humanity.json'
 
 function PensionWallet() {
-  const [walletDesconected, setWalletDesconected] = React.useState(true)
-  const [addressWallet, setAdressWallet] = React.useState('')
+  const [addressWallet, setAdressWallet] = React.useState('Connect your Wallet')
+  const [loading, setLoading] = React.useState(false)
 
   const connectWallet = async () => {
     if (typeof window.ethereum !== 'undefined') {
+      setLoading(true)
       const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
       const accounts = await web3Provider.send('eth_requestAccounts', [])
       const wallet = accounts[0]
       
       const verification = await verifyInProofOfHumanity(wallet)
-      console.log('verify :', verification)
+      //console.log('verify :', verification)
       if (verification) {
         const web3Signer = web3Provider.getSigner()
         const chanId = await web3Signer.getChainId()
         if (chanId !== 4) {
           alert("Change your network to Rinkeby's testnet!")
+          setLoading(false)
           return
         }
-        setWalletDesconected(false)
         setAdressWallet('...' + String(wallet).slice(38))
-        //setDisable(false)
+        setLoading(false)
       } else {
         alert('Your wallet is not registed in Proof of Humanity')
+        setLoading(false)
       }
     } else {
       alert('Install Metamask in your browser')
@@ -42,9 +43,11 @@ function PensionWallet() {
   }
 
   return (
-    <button onClick={connectWallet}>
-      {walletDesconected ? 'Connect your wallet' : addressWallet}
-    </button>
+    <React.Fragment>
+      <button onClick={connectWallet}>
+          {loading ? 'Loading...' : addressWallet}
+      </button>
+    </ React.Fragment>
   )
 }
 
