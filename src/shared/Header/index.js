aa963677d9b99a03a9f2fc6beb95af8821922c7e
Gerardo Miranda <gerardodev25@gmail.com>
2022-08-25 10:11:38 -0400

    [STYLE] was add class active to Header's links

--- src/shared/Header/index.js
+++ src/shared/Header/index.js
@@ -1,51 +1,57 @@
-import './Header.scss'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import './Header.scss';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header(props) {
-  const [isRegisted, setIsRegisted] = React.useState(false)
-  const [isVerified, setIsVerified] = React.useState(false)
+  const [isRegisted, setIsRegisted] = React.useState(false);
+  const [isVerified, setIsVerified] = React.useState(false);
 
-  React.useEffect(() => {
+  React.useEffect(() => {}, []);
 
-  }, [])
-
-  return(
-    <header className='Header'>
+  return (
+    <header className="Header">
       <nav>
-        <ul className='main-nav'>
-          <figure className='main-nav__logo'>
-            <img src='./assets/images/pension.png' alt='logo' />
+        <ul className="main-nav">
+          <figure className="main-nav__logo">
+            <img src="./assets/images/pension.png" alt="logo" />
             <figcaption>Pension</figcaption>
           </figure>
-          <li className='main-nav__item'>
-            <NavLink to='/' >Home</NavLink>
-          </li>
-          <li className='main-nav__item'>
-            <NavLink to='/about' >About</NavLink>
-          </li>
-          {isVerified && !isRegisted && <li className='main-nav__item'>
-            <NavLink to='/contribute' >
-              Contribute
+          <li className="main-nav__item">
+            <NavLink to="/" className={({ isActive }) => (isActive && 'active')}>
+              Home
             </NavLink>
-          </li>}
-          {isVerified && isRegisted  && <li className='main-nav__item'>
-            <NavLink to='/mypension' >
+          </li>
+          <li className="main-nav__item">
+            <NavLink to="/about" className={({ isActive }) => (isActive && 'active')}>
+              About
+            </NavLink>
+          </li>
+          {isVerified && !isRegisted && (
+            <li className="main-nav__item">
+              <NavLink to="/contribute" className={({ isActive }) => (isActive && 'active')}>
+                Contribute
+              </NavLink>
+            </li>
+          )}
+          {isVerified && isRegisted && (
+            <li className="main-nav__item">
+              <NavLink to="/mypension" className={({ isActive }) => (isActive && 'active')}>
                 My pension
-            </NavLink>
-          </li>}
-          {isVerified && !isRegisted && <li className='main-nav__item'>
-            <NavLink to='/register' >
+              </NavLink>
+            </li>
+          )}
+          {isVerified && !isRegisted && (
+            <li className="main-nav__item">
+              <NavLink to="/register" className={({ isActive }) => (isActive && 'active')}>
                 Register
-            </NavLink>
-          </li>}
-          <li className='main-nav__item'>
-            {React.cloneElement(props.children, {setIsRegisted, setIsVerified})}
-          </li>
+              </NavLink>
+            </li>
+          )}
+          <li className="main-nav__item">{React.cloneElement(props.children, { setIsRegisted, setIsVerified })}</li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
 
-export { Header }
+export { Header };
