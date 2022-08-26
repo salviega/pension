914c01e466d1914fbcbde948343711666afe92b3
Gerardo Miranda <gerardodev25@gmail.com>
2022-08-26 12:01:46 -0400

    [FEAT] handle the spinner with redux

--- src/store/types.js
+++ src/store/types.js
@@ -1,6 +1,11 @@
 export const types = {
+  // * modal
   uiModalOpen: '[UI] open modal',
   uiModalClose: '[UI] close modal',
   uiModalLoadDara: '[UI] load data modal',
   uiModalUnloadDara: '[UI] unload data modal',
+  
+  // * spinner
+  uiSpinnerActive: '[UI] Spinner active',
+  uiSpinnerDesactive: '[UI] Spinner desactive',
 };
