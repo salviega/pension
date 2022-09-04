87c687c541d0d92929b8d0062a88152ff7ca54f8
Jaime Jiménez <jimenez.ing.sis@gmail.com>
2022-09-03 20:45:20 -0500

    [FEAT] Added TheGraph functionality. Working query

--- pension/src/contract.ts
+++ pension/src/contract.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
   Contract,
   Approval,
@@ -16,20 +16,23 @@ export function handleTransfer(event: Transfer): void {}
 export function handledepositContributor(event: depositContributor): void {
     // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = _depositContributor.load(event.transaction.from.toHex())
+  let id = event.transaction.hash.toHexString()
+  let entity = _depositContributor.load(id)
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new _depositContributor(event.transaction.from.toHex())
-
+    entity = new _depositContributor(id)
+    //entity.id = BigInt.fromI32(1);
     // Entity fields can be set using simple assignments
     //entity.contributorAddress = event.params.contributorAddress
   }
     // Entity fields can be set based on event parameters
 
-    entity.contributorAmount = entity.contributorAmount.plus(BigInt.fromI32(2))
-  
+    entity.contributorAmount = event.params.contributorAmount
+    entity.timeDeposit = event.params.timeDeposit.plus(BigInt.fromI32(2))
+    entity.contributorAddress = event.params.contributorAddress.toHexString()
+    
     // Entities can be written to the store with `.save()`
     entity.save()
 
