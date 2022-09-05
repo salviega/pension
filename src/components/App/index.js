1c176c1fa86a98938e3b8348f2d49d29608f323f
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-04 21:55:47 -0400

    [FEAT] working on auth system persistence

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,29 +1,106 @@
-import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { ethers } from "ethers";
+// import React from "react";
+// import { HashRouter, Route, Routes } from "react-router-dom";
+// import { ethers } from "ethers";
 
-import { Header } from "../../shared/Header";
-import { Footer } from "../../shared/Footer";
-import { PensionLoading } from "../PensionLoading";
-import { PensionWallet } from "../PensionWallet";
-import { PensionHome } from "../PensionHome";
-import { PensionAbout } from "../PensionAbout";
-import { PensionMyPensions } from "../PensionMyPensions";
-import { PensionRegister } from "../PensionRegister";
+// import { Header } from "../../shared/Header";
+// import { Footer } from "../../shared/Footer";
+// import { PensionLoading } from "../PensionLoading";
+// import { PensionWallet } from "../PensionWallet";
+// import { PensionHome } from "../PensionHome";
+// import { PensionAbout } from "../PensionAbout";
+// import { PensionMyPensions } from "../PensionMyPensions";
+// import { PensionRegister } from "../PensionRegister";
 
-import "./App.scss";
-import { Modal } from "../../shared/Modal";
-import { useSelector } from "react-redux";
+// import "./App.scss";
+// import { Modal } from "../../shared/Modal";
+// import { useSelector } from "react-redux";
+// import { PensionContext } from '../PensionContext';
+// import { PensionError } from '../PensionError';
+
+// function App() {
+//   const {
+//     items: generalBalance,
+//     loading,
+//     error,
+//   } = React.useContext(PensionContext)
+//   const { modal, spinner } = useSelector((e) => e.ui);
+
+//   React.useEffect(() => {
+//     const currentNetwork = async () => {
+//       const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+//       const web3Signer = web3Provider.getSigner();
+//       const chainId = await web3Signer.getChainId();
+//       return chainId;
+//     };
+//     if (window.ethereum) {
+//       window.ethereum.on("chainChanged", () => {
+//         currentNetwork().then((response) => {
+//             if (response !== 4) {
+//                 window.location.reload();
+//               }
+//           });
+//       });
+//     }
+
+//     if (window.ethereum) {
+//       window.ethereum.on("accountsChanged", () => {
+//         alert(
+//           "You changed the account, you were returned to the home page"
+//           );
+//         window.location.reload();
+//       });
+//     }
+//   }, []);
+//   return (
+//     <HashRouter>
+//       <div className="App__container">
+//         <Header>
+//           <PensionWallet />
+//         </Header>
+//         <main>
+//           {error && <PensionError />}
+//           {loading && <PensionLoading />}
+//           <Routes>
+//             <Route path="/" element={<PensionHome />} />
+//             <Route path="/about" element={<PensionAbout />} />
+//             <Route path="/mypensions" element={<PensionMyPensions />} />
+//             <Route path="/register" element={<PensionRegister />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//         {modal.isOpen && <Modal />}
+//       </div>
+//     </HashRouter>
+//   );
+// }
+
+// export { App };
+
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ethers } from 'ethers';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Header } from '../../shared/Header';
+import { Footer } from '../../shared/Footer';
+import { PensionLoading } from '../PensionLoading';
+import { PensionWallet } from '../PensionWallet';
+import { PensionHome } from '../PensionHome';
+import { PensionAbout } from '../PensionAbout';
+import { PensionMyPensions } from '../PensionMyPensions';
+import { PensionRegister } from '../PensionRegister';
+
+import './App.scss';
+import { Modal } from '../../shared/Modal';
 import { PensionContext } from '../PensionContext';
 import { PensionError } from '../PensionError';
+import { authLoguotAction } from '../../store/actions/authAction';
 
 function App() {
-  const {
-    items: generalBalance,
-    loading,
-    error,
-  } = React.useContext(PensionContext)
-  const { modal, spinner } = useSelector((e) => e.ui);
+  const dispatch = useDispatch();
+
+  const { items: generalBalance, loading, error } = React.useContext(PensionContext);
+  const { modal } = useSelector((e) => e.ui);
 
   React.useEffect(() => {
     const currentNetwork = async () => {
@@ -33,24 +110,21 @@ function App() {
       return chainId;
     };
     if (window.ethereum) {
-      window.ethereum.on("chainChanged", () => {
+      window.ethereum.on('chainChanged', () => {
         currentNetwork().then((response) => {
-            if (response !== 4) {
-                window.location.reload();
-              }
-          });
+          if (response !== 4) {
+            dispatch(authLoguotAction());
+            window.location.reload();
+          }
+        });
       });
-    }
-
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", () => {
-        alert(
-          "You changed the account, you were returned to the home page"
-          );
+      window.ethereum.on('accountsChanged', () => {
+        alert('You changed the account, you were returned to the home page');
+        dispatch(authLoguotAction());
         window.location.reload();
       });
     }
-  }, []);
+  }, [dispatch]);
   return (
     <HashRouter>
       <div className="App__container">
