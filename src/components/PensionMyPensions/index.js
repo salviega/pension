b58a9df2e820471b9975e4c848e3af1525c83848
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-04 23:03:34 -0400

    [FEAT] add chart to mypensions view

--- src/components/PensionMyPensions/index.js
+++ src/components/PensionMyPensions/index.js
@@ -1,15 +1,20 @@
 import './PensionMyPensions.scss';
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-import { PensionOfUser } from './PensionOfUser/index.js'
-import { PensionStadistic } from './PensionStadistic/PensionStadistic.js'
+import { PensionOfUser } from './PensionOfUser/index.js';
+import { PensionStadistic } from './PensionStadistic/PensionStadistic.js';
 import { getSubGraphData } from '../../middleware/getSubGraphData';
 
 function PensionMyPensions() {
   const { getAllElementsByItem: getAllQuotesByAddress } = getSubGraphData();
   const { isRegisted, isVerified } = useSelector(({ auth }) => auth);
+
+  const [labels, setLabels] = useState([]);
+  const [data, setData] = useState([]);
+
+  const { wallet } = useSelector(({ auth }) => auth);
 
   const mockPension = [
     {
@@ -23,27 +28,41 @@ function PensionMyPensions() {
       PensionName: 'Pension 2',
       totalContributions: 300000,
       totalReturns: 3600,
-    }
-  ]
+    },
+  ];
 
-  React.useEffect(() => {
-    const data = async (address) => {
-      console.log( await getAllQuotesByAddress(address))
-    }
-    //data(address);
-  }, [])
-  
+  useEffect(() => {
+    const getData = async () => {
+      return await getAllQuotesByAddress(wallet);
+    };
+
+    getData().then((info) => {
+      const infoLavel = [];
+      const infoData = [];
+      for (const item of info) {
+        infoLavel.push(item.timeDeposit);
+        infoData.push(item.contributorAmount);
+      }
+      setData(infoData);
+      setLabels(infoLavel);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wallet]);
+
+  useEffect(() => {
+    console.log({ labels, data });
+  }, [labels, data]);
+
   if (!isVerified || !isRegisted) return <Navigate replace to="/" />;
-  
+
   return (
     <div className="my-pensions-container">
-      <div className='my-pensions-container__pensions-list'>
-        {mockPension.map(pension => (
-          <PensionOfUser key={pension.pensionAddress} {...pension} ></PensionOfUser>
+      <div className="my-pensions-container__pensions-list">
+        {mockPension.map((pension) => (
+          <PensionOfUser key={pension.pensionAddress} {...pension}></PensionOfUser>
         ))}
       </div>
-      <PensionStadistic />
-
+      <PensionStadistic labels={labels} data={data} />
     </div>
   );
 }
