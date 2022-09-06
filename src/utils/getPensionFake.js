90ce63cf20caae33b1271a2cad8ec03801eaff30
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-05 21:33:41 -0400

    [FEAT] working in the chart on home page

--- src/utils/getPensionFake.js
+++ src/utils/getPensionFake.js
@@ -1,12 +1,13 @@
-// import faker from 'faker';
 
 export const getPensionFake = (values) => {
   const labels = [];
   for (let index = 1; index <= 100; index++) {
     labels.push(index);
   }
-  // const data = labels.map(() => faker.datatype.number({ min: 0, max: 500 }));
-  const data = labels.map(() => Math.round(Math.random() * 100));
+
+  const numRandon = Math.round(Math.random() * 10)
+  
+  const data = labels.map((item) => item *numRandon );
 
   return { labels, data };
 };
