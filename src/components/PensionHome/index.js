5de5107510c7e9a9900a1b54f3d86496afed5751
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-06 18:45:21 -0400

    [FEAT] change description on home view

--- src/components/PensionHome/index.js
+++ src/components/PensionHome/index.js
@@ -9,8 +9,8 @@ function PensionHome() {
       <div className="home__header">
         <h1 className="home__title">Pension system</h1>
         <p className="home__description">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo nostrum, veniam nobis quis adipisci asperiores
-          magnam?
+          calculate an approximate value of the amount you would like to have allocated to your pension based on your age, the
+          monetary amount you want to invest monthly and other parameters ...
         </p>
       </div>
       <HomeChart />
