4b96e116a7443c1dedc63c6d7cc24e02f71447f6
Santiago Viana Vera <salviega6@gmail.com>
2022-09-06 09:51:29 -0500

    [FEAT] The mint function is done

--- src/components/PensionRegister/index.js
+++ src/components/PensionRegister/index.js
@@ -1,38 +1,91 @@
-import './PensionRegister.scss';
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { ethers } from 'ethers';
-import { useSelector } from 'react-redux';
-import { useForm } from '../../hooks/useForm';
-import { useState } from 'react';
-import { validateRegisterForm, verifyform } from './validateRegisterForm';
+import "./PensionRegister.scss";
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { ethers } from "ethers";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
+import { useState } from "react";
+import { validateRegisterForm, verifyform } from "./validateRegisterForm";
 
-import jsonPension from '../../blockchain/environment/contract-address.json';
-import pensionContractAbi from '../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/Pension.sol/Pension.json';
+import jsonPension from "../../blockchain/environment/contract-address.json";
+import pensionContractAbi from "../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/Pension.sol/Pension.json";
+import { PensionLoading } from "../PensionLoading";
 const pensionAddress = jsonPension.pensioncontract;
 
-function PensionRegister() {
+function PensionRegister({ loading, setLoading }) {
   const { isRegisted, isVerified } = useSelector(({ auth }) => auth);
-  const [values, handleInputChange] = useForm({ birthDate: '', gender: '', firstDeposite: 25, check: false });
-  const [validate, setValidate] = useState({ birthDate: true, gender: true, firstDeposite: true, check: true });
+  const [values, handleInputChange] = useForm({
+    birthDate: "",
+    gender: "",
+    firstDeposite: 25,
+    check: false,
+  });
+  const [validate, setValidate] = useState({
+    birthDate: true,
+    gender: true,
+    firstDeposite: true,
+    check: true,
+  });
   const { birthDate, firstDeposite } = values;
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setValidate({ ...validate, ...validateRegisterForm(values) });
-    console.log('form is: ' + verifyform(validate));
-
-    if (!verifyform(validate)) return;
 
     const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
     const web3Signer = web3Provider.getSigner();
-    const pensionContract = new ethers.Contract(pensionAddress, pensionContractAbi.abi, web3Signer);
-    console.log(pensionContract);
+    const pensionContract = new ethers.Contract(
+      pensionAddress,
+      pensionContractAbi.abi,
+      web3Signer
+    );
+
+    const biologySex = values.gender;
+    let date = new Date();
+    let now = date.getFullYear();
+    const age = now - values.birthDate.split("-")[0];
+    let datum = new Date(
+      Date.UTC(
+        values.birthDate.split("-")[0],
+        values.birthDate.split("-")[1],
+        values.birthDate.split("-")[2]
+      )
+    );
+    datum = datum.getTime() / 1000;
+    const bornAge = datum;
+    const firstQuote = values.firstDeposite;
+
+    try {
+      const response = await pensionContract.safeMint(
+        biologySex,
+        age,
+        bornAge,
+        firstQuote,
+        { value: firstQuote.toString() }
+      );
+
+      web3Provider
+        .waitForTransaction(response.hash)
+        .then((_response) => {
+          setLoading(false);
+          alert("Successful transaction");
+        })
+        .catch((_error) => {
+          setLoading(false);
+          alert("Failed transaction");
+        });
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      alert("Failed transaction");
+    }
   };
 
-  //if (isVerified || !isRegisted) return <Navigate replace to="/" />;
+  if (isVerified || !isRegisted) return <Navigate replace to="/" />;
+
   return (
     <React.Fragment>
+      {loading && <PensionLoading />}
       <h2>Get your pension</h2>
       <div className="container">
         <form onSubmit={handleSubmit}>
@@ -41,20 +94,47 @@ function PensionRegister() {
               <h4>Date of Birth</h4>
               <div className="input-group">
                 <div className="col-third">
-                  <input type="date" name="birthDate" id="birthDate" value={birthDate} onChange={handleInputChange} />
-                  <p className={`error-msg ${validate.birthDate && 'error-msg--hidde'}`}>required field, minimum age 18 years</p>
+                  <input
+                    type="date"
+                    name="birthDate"
+                    id="birthDate"
+                    value={birthDate}
+                    onChange={handleInputChange}
+                  />
+                  <p
+                    className={`error-msg ${
+                      validate.birthDate && "error-msg--hidde"
+                    }`}
+                  >
+                    required field, minimum age 18 years
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-half">
               <h4>Gender</h4>
               <div className="input-group">
-                <input id="gender-male" type="radio" name="gender" value="male" onChange={handleInputChange} />
+                <input
+                  id="gender-male"
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  onChange={handleInputChange}
+                />
                 <label htmlFor="gender-male">Male</label>
-                <input id="gender-female" type="radio" name="gender" value="female" onChange={handleInputChange} />
+                <input
+                  id="gender-female"
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  onChange={handleInputChange}
+                />
                 <label htmlFor="gender-female">Female</label>
               </div>
-              <p className={`error-msg ${validate.gender && 'error-msg--hidde'}`} style={{ marginTop: '6.5rem' }}>
+              <p
+                className={`error-msg ${validate.gender && "error-msg--hidde"}`}
+                style={{ marginTop: "6.5rem" }}
+              >
                 required field
               </p>
             </div>
@@ -69,20 +149,34 @@ function PensionRegister() {
                 min={25}
                 placeholder="Greater than or equal to $25 wei"
                 name="firstDeposite"
-                value={firstDeposite}
                 onChange={handleInputChange}
               />
-              <p className={`error-msg ${validate.firstDeposite && 'error-msg--hidde'}`}>required field</p>
+              <p
+                className={`error-msg ${
+                  validate.firstDeposite && "error-msg--hidde"
+                }`}
+              >
+                required field
+              </p>
             </div>
             <div className="input-group">
-              <input id="terms" type="checkbox" name="check" onChange={handleInputChange} />
+              <input
+                id="terms"
+                type="checkbox"
+                name="check"
+                onChange={handleInputChange}
+              />
               <label className="space" htmlFor="terms">
                 Not your keys, not your coins
               </label>
-              <p className={`error-msg ${validate.check && 'error-msg--hidde'}`}>required check</p>
+              <p
+                className={`error-msg ${validate.check && "error-msg--hidde"}`}
+              >
+                required check
+              </p>
             </div>
             <button type="submit" className="button-summit">
-              Create
+              Mint
             </button>
           </div>
         </form>
